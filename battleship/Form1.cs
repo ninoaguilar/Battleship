5b419c0800e178cddd1b1861b28439a29e580322
Brandt Henslee <brandthenslee@gmail.com>
2015-11-08 14:53:31 -0700

    Start Screen Commit

--- battleship/Form1.cs
+++ battleship/Form1.cs
@@ -12,9 +12,26 @@ namespace battleship
 {
     public partial class Form1 : Form
     {
+        WMPLib.WindowsMediaPlayer musicPlayer = new WMPLib.WindowsMediaPlayer();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            musicPlayer.URL = (@"StartMusic.mp3");
+            musicPlayer.settings.volume = 15;
+            musicPlayer.controls.play();
+        }
+
+        private void beginButton_Click(object sender, EventArgs e)
+        {
+            startScreen.Enabled = false;
+            startScreen.Visible = false;
+            beginButton.Enabled = false;
+            beginButton.Visible = false;
+            musicPlayer.controls.stop();
         }
     }
 }
