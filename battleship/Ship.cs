1e1df56158e8e2306344fb770d17505d77c197ad
Jeffrey <jeffrey@customerhook.com>
2015-11-18 18:13:16 -0700

    Here's the real Player and Ship class.

---
+++ battleship/Ship.cs
@@ -0,0 +1,89 @@
+﻿namespace battleship
+{
+
+    public class Ship
+    {
+        private Square[] position;
+        private int length;
+        private bool isSunk;
+
+
+        public Ship(int shipLength)
+        {
+            this.position = new Square[shipLength];
+            this.length = shipLength;
+            this.isSunk = false;
+        }
+
+        public Square[] Position
+        {
+            get
+            {
+                return position;
+            }
+
+            set
+            {
+                position = value;
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+
+            set
+            {
+                length = value;
+            }
+        }
+
+        public bool IsSunk
+        {
+            get
+            {
+                return isSunk;
+            }
+
+            set
+            {
+                isSunk = value;
+            }
+        }
+
+        public void takeHit()
+        {
+            foreach (var square in this.position)   //Check if squares are hit.
+            {
+                if(square.State.hit)
+                {
+                    this.sink();        //Check if ship is sunk.
+                }
+            }
+        }
+
+        public void sink()
+        {
+            bool sink = true;
+            foreach (var square in this.Position)
+            {
+                    if (!square.State.hit)
+                    {
+                        sink = false;
+                    }
+            }
+            if(sink)
+            {
+                this.isSunk = true;
+            }
+            else
+            {
+                this.isSunk = false;
+            }
+            //If each square in position array has value 'hit', change isSunk to true.
+        }
+    }
+}
